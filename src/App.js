cd76ce0d4bccc19af3aedf89cf3f955375301dc8
Justin KA <justinalexander101@gmail.com>
2023-07-23 15:42:43 -0400

    search and retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import React, {useState} from 'react'
 import './App.css';
+import CitySearch from './CitySearch';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AirQualityCard from './AirQualityCard';
 
 function App() {
+  const [airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null)
+
+
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+      const data = await response.json()
+      console.log(data)
+      if(response.ok && data.status ==='ok') {
+        setAirQualityData(data.data)
+        setError(null)
+      } else {
+        setError('Couldnt find city or there was a typo. Please check and try again.')
+      }
+    } catch (error) {
+      console.error('network error:', error)
+      setError('Sorry something went wrong')
+      setAirQualityData(null)
+    }
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div className='container'>
+    <h1 className='mt-5 mb-3'>My City Air Quality</h1>
+    <CitySearch getAirQuality={getAirQuality}/>
+    {error && (
+      <div className='alert alert-danger' role='alert'>
+        {error}
+      </div>
+    )}
+    {airQualityData && (
+      //Pollutant info
+      <>
+      <AirQualityCard data={airQualityData}/>
+      
+      </>
+    )}
+    </div>
   );
 }
 
