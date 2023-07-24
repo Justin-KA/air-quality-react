c4ff382d57e5df9300e10863f7e85200070d7281
Justin KA <justinalexander101@gmail.com>
2023-07-24 19:53:08 -0400

    added styling and info cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
-import './App.css';
 import CitySearch from './CitySearch';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AirQualityCard from './AirQualityCard';
+import PollutantInfo from './PollutantInfo';
+import './App.css';
 
 function App() {
   const [airQualityData, setAirQualityData] = useState(null)
@@ -40,7 +41,7 @@ function App() {
       //Pollutant info
       <>
       <AirQualityCard data={airQualityData}/>
-      
+      <PollutantInfo pollutant={airQualityData.dominentpol}/>
       </>
     )}
     </div>
